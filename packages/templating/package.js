d607fa09ea8a48f05c7db873a614526a8d033fdd
Nathan Reid <nathan-code@nathantreid.com>
2016-07-28 17:10:53 -0500

    add blaze packages with proper version bumps so Meteor will run

---
+++ packages/templating/package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: "Allows templates to be defined in .html files",
+  version: '1.1.14'
+});
+
+// Today, this package is closely intertwined with Handlebars, meaning
+// that other templating systems will need to duplicate this logic. In
+// the future, perhaps we should have the concept of a template system
+// registry and a default templating system, ideally per-package.
+
+Package.onUse(function (api) {
+  api.use('templating-runtime');
+  api.export('Template', 'client');
+
+  api.imply('templating-compiler');
+  api.imply('templating-runtime');
+});
